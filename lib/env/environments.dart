91a5e0a6d9553ed1f66995c1d8cf65e107aa2c4f
Julio Cons <jconscalderon@hotmail.com>
2022-02-09 18:42:29 -0700

    🔧 CONFIG:  Add environmets variable

---
+++ lib/env/environments.dart
@@ -0,0 +1,7 @@
+class Enviroments {
+  static const _apiUrl = 'https://api.giphy.com/v1/gifs/search';
+  static const _apiKey = 'Px8KFrgrApOvNH9wPuG0fv47d2SzsnrO';
+
+  static String get apiUrl => _apiUrl;
+  static String get apiKey => _apiKey;
+}
