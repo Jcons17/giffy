731e4fddff83ddf14b8b2a5e1f1dfb12177ef638
Julio Cons <jconscalderon@hotmail.com>
2022-02-09 21:48:33 -0700

    add bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:giffy/bloc/keywoard/bloc/keywoard_bloc.dart';
 import 'package:giffy/screen/home/home.dart';
 import 'package:giffy/themes/color.dart';
 
@@ -9,20 +11,23 @@ class GiffyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Giffy',
-        home: const Home(),
-        themeMode: ThemeMode.dark,
-        darkTheme: ThemeData(
-          scaffoldBackgroundColor: bgColorDark,
-          inputDecorationTheme: const InputDecorationTheme(
-              border: InputBorder.none,
-              constraints: BoxConstraints(maxWidth: 300, maxHeight: 50),
-              contentPadding: EdgeInsets.all(8)),
-          textTheme: const TextTheme(
-            bodyMedium: TextStyle(color: Colors.white),
-          ),
-        ));
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (_) => KeywoardBloc())],
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Giffy',
+          home: const Home(),
+          themeMode: ThemeMode.dark,
+          darkTheme: ThemeData(
+            scaffoldBackgroundColor: bgColorDark,
+            inputDecorationTheme: const InputDecorationTheme(
+                border: InputBorder.none,
+                constraints: BoxConstraints(maxWidth: 300, maxHeight: 50),
+                contentPadding: EdgeInsets.all(8)),
+            textTheme: const TextTheme(
+              bodyMedium: TextStyle(color: Colors.white),
+            ),
+          )),
+    );
   }
 }
