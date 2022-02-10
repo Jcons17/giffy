34e8462029722f5829e5ec879f559ae34f1c8258
Julio Cons <jconscalderon@hotmail.com>
2022-02-09 18:44:03 -0700

    ✨ NEW FEATURE:  Add input button

--- lib/screen/home/home.dart
+++ lib/screen/home/home.dart
@@ -1,5 +1,10 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:giffy/screen/home/header_home.dart';
+import 'package:giffy/screen/home/list_of_gifs.dart';
+import 'package:giffy/widgets/input/input_file.dart';
+import 'package:giffy/widgets/input/submit_keyword.dart';
 import 'package:giffy/widgets/text/text_body.dart';
 
 class Home extends StatelessWidget {
@@ -8,11 +13,44 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: const [
-          HeaderHome(),
-        ],
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
+            HeaderHome(),
+            SizedBox(height: 30),
+            FormKeyword(),
+            SizedBox(height: 30),
+            ListOfGifs()
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class FormKeyword extends StatelessWidget {
+  const FormKeyword({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50,
+      child: Form(
+          child: ClipRect(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: const [
+            InputKeyword(),
+            SubmitButton(),
+          ],
+        ),
+      )),
     );
   }
 }
---
+++ lib/widgets/input/submit_keyword.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class SubmitButton extends StatefulWidget {
+  const SubmitButton({Key? key}) : super(key: key);
+
+  @override
+  State<SubmitButton> createState() => _SubmitButtonState();
+}
+
+class _SubmitButtonState extends State<SubmitButton> {
+  bool isHover = false;
+  bool isPress = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () async => handlePress(),
+      onHover: (hover) {
+        setState(() {
+          isHover = hover;
+        });
+      },
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 100),
+        padding: EdgeInsets.all(8),
+        color: !isHover
+            ? Colors.deepPurple
+            : isPress
+                ? Colors.deepPurple.shade100
+                : Colors.deepPurple.shade800,
+        height: 50,
+        child: const Center(child: Text("Buscar...")),
+      ),
+    );
+  }
+
+  handlePress() async {}
+}
